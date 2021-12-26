fbddfc7949d1f79ff0320d92e3b2ecb575bed583
197PM09434 - Trần Thành Đạt - K25T-PM <tdat1155@gmail.com>
2021-12-26 10:22:17 +0700

    update project

--- TranThanhDat_Exercises2/Controllers/CategoryController.cs
+++ TranThanhDat_Exercises2/Controllers/CategoryController.cs
@@ -12,25 +12,25 @@ namespace TranThanhDat_Exercises2.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        private MyDbContext _context;
-        private IMapper _mapper;
+        private readonly MyDbContext _context;
+        private readonly IMapper _mapper;
 
-        public CategoryController(MyDbContext context, IMapper mapper )
+        public CategoryController(MyDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var categories = _context.Categories.ToList();
+            var categories = await _context.Categories.ToListAsync();
             var model = _mapper.Map<List<Category>, List<CategoryViewModel>>(categories);
             return Ok(model);
         }
 
         [HttpPost("{name}")]
-        public IActionResult Add(string name)
+        public async Task<IActionResult> Add(string name)
         {
             try
             {
@@ -38,8 +38,8 @@ namespace TranThanhDat_Exercises2.Controllers
                 {
                     Name = name
                 };
-                _context.Categories.Add(category);
-                _context.SaveChanges();
+                await _context.Categories.AddAsync(category);
+                await _context.SaveChangesAsync();
 
                 var model = _mapper.Map<CategoryViewModel>(category);
                 return StatusCode(StatusCodes.Status201Created, model);
@@ -51,40 +51,40 @@ namespace TranThanhDat_Exercises2.Controllers
         }
 
         [HttpPut("{id}")]
-        public IActionResult Edit(int id, CategoryViewModel categoryViewModel)
+        public async Task<IActionResult> Edit(int id, CategoryViewModel categoryViewModel)
         {
             if (id != categoryViewModel.Id)
             {
                 return BadRequest();
             }
 
-            var category = _context.Categories.SingleOrDefault(c => c.Id == id);
-            if(category == null)
+            var category = await _context.Categories.SingleOrDefaultAsync(c => c.Id == id);
+            if (category == null)
             {
                 return NotFound();
             }
 
             category.Name = categoryViewModel.Name;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
-            if(category == null)
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
             {
                 return NotFound();
             }
 
             _context.Categories.Remove(category);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return Ok();
         }
 
 
-    
+
     }
 }
--- TranThanhDat_Exercises2/Controllers/ProductController.cs
+++ TranThanhDat_Exercises2/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using TranThanhDat_Exercises2.Data;
 using TranThanhDat_Exercises2.Data.Entities;
 using TranThanhDat_Exercises2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TranThanhDat_Exercises2.Controllers
 {
@@ -11,8 +12,8 @@ namespace TranThanhDat_Exercises2.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private IMapper _mapper;
-        private MyDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly MyDbContext _context;
 
         public ProductController(IMapper mapper, MyDbContext context)
         {
@@ -21,9 +22,9 @@ namespace TranThanhDat_Exercises2.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById(String id)
+        public async Task<IActionResult> GetById(String id)
         {
-            var produuct = _context.Products.SingleOrDefault(x=>x.Id.ToString() == id);
+            var produuct = await _context.Products.SingleOrDefaultAsync(x => x.Id.ToString() == id);
             if (produuct == null)
             {
                 return NotFound();
@@ -35,9 +36,9 @@ namespace TranThanhDat_Exercises2.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var products = _context.Products.ToList();
+            var products = await _context.Products.ToListAsync();
             var listProductVM = _mapper.Map<List<Product>, List<ProductViewModel>>(products);
             return Ok(listProductVM);
         }
@@ -45,11 +46,11 @@ namespace TranThanhDat_Exercises2.Controllers
         [HttpGet("keySearch")]
         public IActionResult Search(string keySearch, double? from, double? to, string? sortBy)
         {
-            var products = _context.Products.Where(x=> x.Name.Contains(keySearch));
+            var products = _context.Products.Where(x => x.Name.Contains(keySearch));
 
             if (from.HasValue)
             {
-                products = products.Where(x=>x.Price >= from);
+                products = products.Where(x => x.Price >= from);
             }
             if (to.HasValue)
             {
@@ -82,15 +83,15 @@ namespace TranThanhDat_Exercises2.Controllers
 
 
         [HttpPost]
-        public IActionResult Add(ProductModel productModel)
+        public async Task<IActionResult> Add(ProductModel productModel)
         {
             try
             {
                 var product = _mapper.Map<Product>(productModel);
 
                 product.Id = Guid.NewGuid();
-                _context.Products.Add(product);
-                _context.SaveChanges();
+                await _context.Products.AddAsync(product);
+                await _context.SaveChangesAsync();
 
 
                 var model = _mapper.Map<ProductModel>(product);
@@ -103,9 +104,9 @@ namespace TranThanhDat_Exercises2.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(String id)
+        public async Task<IActionResult> Delete(String id)
         {
-            var product = _context.Products.FirstOrDefault(x => x.Id.ToString() == id);
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id.ToString().Equals(id));
             if (product == null)
             {
                 return NotFound();
@@ -113,32 +114,32 @@ namespace TranThanhDat_Exercises2.Controllers
             }
 
             _context.Products.Remove(product);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok();
         }
 
         [HttpPut]
-        public IActionResult Edit(String id, ProductViewModel productViewModel)
+        public async Task<IActionResult> Edit(String id, ProductViewModel productViewModel)
         {
             if (id != productViewModel.Id.ToString())
             {
                 return BadRequest();
             }
 
-            var product = _context.Products.FirstOrDefault(x => x.Id.ToString() == id);
-            if(product == null)
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id.ToString() == id);
+            if (product == null)
             {
                 return NotFound();
             }
 
             _mapper.Map<ProductViewModel, Product>(productViewModel, product);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
 
 
-        
-       
+
+
     }
 }
